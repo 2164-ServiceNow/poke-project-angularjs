a348004644036c4381bcef519fd44088b98d5552
Charles Jester <charles.jester@revature.com>
2025-01-15 12:45:54 -0500

    during moves implementation decided to include some comments to differentiate watch in itemDetails

--- components/itemDetails/itemDetails.js
+++ components/itemDetails/itemDetails.js
@@ -6,15 +6,17 @@ angular.module('itemDetails', [])
         },
         controller: function($scope, $http){
            
-            $scope.$watch('$ctrl.item', function(newItem){
-                $http.get(newItem.url)
-                    .then((response) =>{
-                        console.log(response.data)
-                        let item = response.data;
-                        $scope.shortEffect = item.effect_entries[0].short_effect
-                        $scope.flavorTextEntries = item.flavor_text_entries[0].text
-                        $scope.sprites = item.sprites.default
-                    })
+            $scope.$watch(
+                '$ctrl.item', // first argument/observable action occurs we trigger the subsequent function with the value returned from the previous argument
+                function(newItem){
+                    $http.get(newItem.url)
+                        .then((response) =>{
+                            console.log(response.data)
+                            let item = response.data;
+                            $scope.shortEffect = item.effect_entries[0].short_effect
+                            $scope.flavorTextEntries = item.flavor_text_entries[0].text
+                            $scope.sprites = item.sprites.default
+                        })
             })
         }
     })
